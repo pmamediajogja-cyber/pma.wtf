89cf86826786ee347be3e70a37843e9450af6bf4
pmamediajogja-cyber <pma.media.jogja@gmail.com>
2026-09-15 13:09:26 +0700

    feat: route notary ppat demo

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,22 @@ import Tools from './components/Tools'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import NotaryDemo from './components/NotaryDemo'
 
-function App() {
+function PortfolioApp() {
   return (
     <div className="bg-dark" style={{ minHeight: '100%', overflow: 'hidden', position: 'relative' }}>
-      {/* Cyber Grid Background */}
       <div className="bg-cyber-grid" style={{ position: 'fixed', inset: 0, opacity: 0.3, pointerEvents: 'none' }} />
-      
       <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <CVSection />
-        <Tools />
-        <Portfolio />
-        <Contact />
-      </main>
+      <main><Hero /><About /><CVSection /><Tools /><Portfolio /><Contact /></main>
       <Footer />
     </div>
   )
+}
+
+function App() {
+  const isNotaryDemo = window.location.pathname.replace(/\/$/, '') === '/demo/notaris'
+  return isNotaryDemo ? <NotaryDemo /> : <PortfolioApp />
 }
 
 export default App
