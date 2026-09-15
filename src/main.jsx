477fffc40000804e08dbf3f903b2a91eee0e81b0
pmamediajogja-cyber <pma.media.jogja@gmail.com>
2026-09-15 11:31:42 +0700

    feat: load homepage visual system

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './journal.css'
 import './services.css'
+import './homepageVisual.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
